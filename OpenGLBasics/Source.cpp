2869abe18f67d8043a367e052611fef0eb298ecc
Карл фон Габсбург <budowniczy.zamyslow@gmail.com>
2017-04-23 19:08:14 +0500

    Ультимативный костылизм

--- OpenGLBasics/Source.cpp
+++ OpenGLBasics/Source.cpp
@@ -2,18 +2,93 @@
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
 #include "Window.h"
 #include "common\shader.hpp"
 
+static const GLfloat g_vertex_buffer_data[] = {
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f
+};
 
-int main();
+static const GLfloat g_color_buffer_data[] = {
+	0.583f,  0.771f,  0.014f,
+	0.609f,  0.115f,  0.436f,
+	0.327f,  0.483f,  0.844f,
+	0.822f,  0.569f,  0.201f,
+	0.435f,  0.602f,  0.223f,
+	0.310f,  0.747f,  0.185f,
+	0.597f,  0.770f,  0.761f,
+	0.559f,  0.436f,  0.730f,
+	0.359f,  0.583f,  0.152f,
+	0.483f,  0.596f,  0.789f,
+	0.559f,  0.861f,  0.639f,
+	0.195f,  0.548f,  0.859f,
+	0.014f,  0.184f,  0.576f,
+	0.771f,  0.328f,  0.970f,
+	0.406f,  0.615f,  0.116f,
+	0.676f,  0.977f,  0.133f,
+	0.971f,  0.572f,  0.833f,
+	0.140f,  0.616f,  0.489f,
+	0.997f,  0.513f,  0.064f,
+	0.945f,  0.719f,  0.592f,
+	0.543f,  0.021f,  0.978f,
+	0.279f,  0.317f,  0.505f,
+	0.167f,  0.620f,  0.077f,
+	0.347f,  0.857f,  0.137f,
+	0.055f,  0.953f,  0.042f,
+	0.714f,  0.505f,  0.345f,
+	0.783f,  0.290f,  0.734f,
+	0.722f,  0.645f,  0.174f,
+	0.302f,  0.455f,  0.848f,
+	0.225f,  0.587f,  0.040f,
+	0.517f,  0.713f,  0.338f,
+	0.053f,  0.959f,  0.120f,
+	0.393f,  0.621f,  0.362f,
+	0.673f,  0.211f,  0.457f,
+	0.820f,  0.883f,  0.371f,
+	0.982f,  0.099f,  0.879f
+};
 
 void initGLFW();
 void initGLEW();
-void initVAO();
 
 void initInputListeners(GLFWwindow * wnd);
 void handleKeyInput(GLFWwindow* window, int key, int scancode, int action, int mods);
@@ -22,13 +97,13 @@ void handleMouseInput(GLFWwindow* window, int button, int action, int mods);
 
 GLuint vertexArrayObject;
 
-bool isRotationMode = false, isTranslationMode = false;
+bool isCameraRotationLeft = false, isCameraRotationRight = false;
 
 int main() {
 	// init Window Object and input handlers
 	initGLFW();
 	Window window;
-	bool isWindowCreated = window.create("Computer Graphics", 800, 600) == 0;;
+	bool isWindowCreated = window.create("Computer Graphics", 600, 600) == 0;;
 	if (!isWindowCreated) return -1;
 	initGLEW();
 	initInputListeners(window.get());
@@ -42,93 +117,6 @@ int main() {
 	// init Shaders
 	GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader");
 	
-	GLuint matrixID = glGetUniformLocation(programID, "MVP");
-	glm::mat4 projection = glm::perspective(45.0f, window.getScreenRatio(), 0.1f, 100.f);
-	glm::mat4 view = glm::lookAt(
-		glm::vec3(4, 3, -3), // Camera is at (4,3,3), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-	);
-	glm::mat4 model = glm::mat4(1.0f); // model is in the center of the world
-	glm::mat4 MVP = projection * view * model;
-
-	static const GLfloat g_vertex_buffer_data[] = {
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f
-	};
-
-	static const GLfloat g_color_buffer_data[] = {
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-		0.597f,  0.770f,  0.761f,
-		0.559f,  0.436f,  0.730f,
-		0.359f,  0.583f,  0.152f,
-		0.483f,  0.596f,  0.789f,
-		0.559f,  0.861f,  0.639f,
-		0.195f,  0.548f,  0.859f,
-		0.014f,  0.184f,  0.576f,
-		0.771f,  0.328f,  0.970f,
-		0.406f,  0.615f,  0.116f,
-		0.676f,  0.977f,  0.133f,
-		0.971f,  0.572f,  0.833f,
-		0.140f,  0.616f,  0.489f,
-		0.997f,  0.513f,  0.064f,
-		0.945f,  0.719f,  0.592f,
-		0.543f,  0.021f,  0.978f,
-		0.279f,  0.317f,  0.505f,
-		0.167f,  0.620f,  0.077f,
-		0.347f,  0.857f,  0.137f,
-		0.055f,  0.953f,  0.042f,
-		0.714f,  0.505f,  0.345f,
-		0.783f,  0.290f,  0.734f,
-		0.722f,  0.645f,  0.174f,
-		0.302f,  0.455f,  0.848f,
-		0.225f,  0.587f,  0.040f,
-		0.517f,  0.713f,  0.338f,
-		0.053f,  0.959f,  0.120f,
-		0.393f,  0.621f,  0.362f,
-		0.673f,  0.211f,  0.457f,
-		0.820f,  0.883f,  0.371f,
-		0.982f,  0.099f,  0.879f
-	};
 	// init and bind Vertex Buffer Object
 	GLuint vertexBuffer;
 	glGenBuffers(1, &vertexBuffer);
@@ -147,6 +135,17 @@ int main() {
 		window.drawBackgroundColor(ColorRGBA::BLUE);
 		glUseProgram(programID);
 
+		GLuint matrixID = glGetUniformLocation(programID, "MVP");
+		glm::mat4 projection = glm::perspective(45.0f, window.getScreenRatio(), 0.1f, 100.f);
+		//glm::mat4 projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f);
+		static glm::vec3 camPos = { 4, 3,-3 }, camTarget = { 0, 0, 0 }, camHead = { 0, 1, 0 }; 
+		if (isCameraRotationLeft)
+			camPos = glm::rotateY(camPos, -0.01f);
+		if (isCameraRotationRight)
+			camPos = glm::rotateY(camPos, 0.01f);
+		glm::mat4 view = glm::lookAt(camPos, camTarget, camHead);
+		glm::mat4 model = glm::mat4(1.0f); // model is in the center of the world
+		glm::mat4 MVP = projection * view * model;
 		glUniformMatrix4fv(matrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		glEnableVertexAttribArray(0);
@@ -189,35 +188,6 @@ void initGLEW() {
 	}
 }
 
-void initVAO() {
-	glGenVertexArrays(1, &vertexArrayObject);
-	glBindVertexArray(vertexArrayObject);
-}
-
-/*void initVertexBuffer() {
-	glGenBuffers(1, &vertexBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-}
-
-void initColorBuffer()
-{
-	glGenBuffers(1, &colorBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-}
-
-void disposeVAO() {
-	glDeleteVertexArrays(1, &vertexArrayObject);
-}
-
-void disposeVertexBuffer() {
-	glDeleteBuffers(1, &vertexBuffer);
-}
-
-void disposeColorBuffer()
-{
-	glDeleteBuffers(1, &colorBuffer);
-}*/
-
 void initInputListeners(GLFWwindow * wnd)
 {
 	glfwSetInputMode(wnd, GLFW_STICKY_KEYS, GL_TRUE); // Ensure we can capture the escape key being pressed
@@ -231,12 +201,16 @@ void handleKeyInput(GLFWwindow* window, int key, int scancode, int action, int m
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	if (key == GLFW_KEY_SPACE)
+	if (key == GLFW_KEY_LEFT)
 		switch (action) {
-		case GLFW_PRESS: isTranslationMode = true; break;
-		case GLFW_RELEASE: isTranslationMode = false; break;
+		case GLFW_PRESS: isCameraRotationLeft = true; break;
+		case GLFW_RELEASE: isCameraRotationLeft = false; break;
 		}
-		
+	if (key == GLFW_KEY_RIGHT)
+		switch (action) {
+		case GLFW_PRESS: isCameraRotationRight = true; break;
+		case GLFW_RELEASE: isCameraRotationRight = false; break;
+		}
 }
 
 void handleMouseMovement(GLFWwindow * window, double xpos, double ypos)
@@ -246,8 +220,8 @@ void handleMouseMovement(GLFWwindow * window, double xpos, double ypos)
 
 void handleMouseInput(GLFWwindow * window, int button, int action, int mods)
 {
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-		isRotationMode = true;
-	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-		isRotationMode = false;
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS);
+		//isRotationMode = true;
+	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS);
+		//isRotationMode = false;
 }
